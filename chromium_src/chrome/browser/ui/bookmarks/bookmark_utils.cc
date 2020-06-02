613a688b498739688a6af720fcad1bbebb6f88a3
Simon Hong <shong@brave.com>
2020-06-02 17:25:53 +0900

    Replace bookmark folder icon in bookmark manager and bar

--- chromium_src/chrome/browser/ui/bookmarks/bookmark_utils.cc
+++ chromium_src/chrome/browser/ui/bookmarks/bookmark_utils.cc
@@ -1,12 +1,26 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "chrome/grit/theme_resources.h"
+#include "ui/base/resource/resource_bundle.h"
+
 #define IsAppsShortcutEnabled IsAppsShortcutEnabled_Unused
-#define ShouldShowAppsShortcutInBookmarkBar ShouldShowAppsShortcutInBookmarkBar_Unused
+#define ShouldShowAppsShortcutInBookmarkBar \
+    ShouldShowAppsShortcutInBookmarkBar_Unused
+
+#if defined(TOOLKIT_VIEWS)
+#define GetBookmarkFolderIcon GetBookmarkFolderIcon_Chromium
+#endif
+
 #include "../../../../../chrome/browser/ui/bookmarks/bookmark_utils.cc"
 #undef IsAppsShortcutEnabled
 #undef ShouldShowAppsShortcutInBookmarkBar
+
+#if defined(TOOLKIT_VIEWS)
+#undef GetBookmarkFolderIcon
+#endif
 
 namespace chrome {
 
@@ -18,4 +32,16 @@ bool ShouldShowAppsShortcutInBookmarkBar(Profile* profile) {
   return false;
 }
 
-} // namespace chrome
+#if defined(TOOLKIT_VIEWS)
+gfx::ImageSkia GetBookmarkFolderIcon(SkColor text_color) {
+#if defined(OS_WIN) || defined(OS_MACOSX)
+  return GetBookmarkFolderIcon_Chromium(text_color);
+#else
+  gfx::ImageSkia folder = *ui::ResourceBundle::GetSharedInstance().
+      GetImageSkiaNamed(IDR_BRAVE_BOOKMARK_FOLDER_CLOSED);
+  return gfx::ImageSkia(std::make_unique<RTLFlipSource>(folder), folder.size());
+#endif
+}
+#endif
+
+}  // namespace chrome
